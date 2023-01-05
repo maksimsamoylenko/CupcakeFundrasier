390f80e18ba6538f0ae160cb5ae8e7bf842881ec
Maksim Samoylenko <samoymaksim@gmail.com>
2023-01-05 17:16:05 -0500

    Added cupcake flavor choices

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcake fundraiser</title>
+</head>
+<body>
+
+<h1> Cupcake Choices...</h1>
+<form method="post" action="confirm.php">
+    <label><input type="checkbox" value="grasshopper" name="flavor[]">The Grasshopper</label>
+    <label><input type="checkbox" value="whiskey" name="flavor[]">Whiskey Maple Bacon</label>
+    <label><input type="checkbox" value="carrotwalnut" name="flavor[]">Carrot Walnut</label>
+    <label><input type="checkbox" value="saltedcaramel" name="flavor[]">Salted Caramel Cupcake</label>
+    <label><input type="checkbox" value="redvelvet" name="flavor[]">Red Velvet</label>
+    <label><input type="checkbox" value="lemondrop" name="flavor[]">Lemon Drop</label>
+    <label><input type="checkbox" value="tiramisu" name="flavor[]">Tiramisu</label>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
